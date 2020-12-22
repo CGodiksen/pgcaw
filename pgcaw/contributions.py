2596f20e2c09ddb6d43f61b47f93cc31bda1c06e
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2020-12-22 17:41:31 +0100

    Added function for getting daily contributions

--- pgcaw/contributions.py
+++ pgcaw/contributions.py
@@ -9,12 +9,19 @@ class Contributions:
         self.contributions_soup = BeautifulSoup(r.text, "html.parser")
 
     def total_contributions(self):
+        """Return the total contributions in the last year."""
         # Find the h2 title text that contains the total contributions.
         title = self.contributions_soup.find("h2").text
 
         # Extracting the number from the title.
         return int(title.strip().split("contributions")[0].strip().replace(",", ""))
 
+    def daily_contributions(self):
+        """
+        Return a list of tuples, each with the format (date, number_contributions_on_date). The list has an element
+        for each day in the last year.
+        """
+        
 
 test = Contributions("cgodiksen")
 print(test.total_contributions())
