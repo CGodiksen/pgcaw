91c0f46c1f21cc68deac96518268691352b8f41c
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2020-12-22 17:36:21 +0100

    Added function to get total contributions.

--- pgcaw/contributions.py
+++ pgcaw/contributions.py
@@ -6,5 +6,15 @@ class Contributions:
     def __init__(self, username):
         url = f"https://github.com/users/{username}/contributions"
         r = requests.get(url)
-
         self.contributions_soup = BeautifulSoup(r.text, "html.parser")
+
+    def total_contributions(self):
+        # Find the h2 title text that contains the total contributions.
+        title = self.contributions_soup.find("h2").text
+
+        # Extracting the number from the title.
+        return int(title.strip().split("contributions")[0].strip().replace(",", ""))
+
+
+test = Contributions("cgodiksen")
+print(test.total_contributions())
